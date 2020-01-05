3423c86f5f0dc455cd6d3a504ca27d9bc530bbc9
Stas <stas@natalenko.com>
2020-01-05 18:51:52 +0200

    Export JS classes in type definitions

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+export * from "./types/interfaces/TClassOrFactory";
+export * from "./types/classes/Container";
+export * from "./types/classes/ContainerBuilder";
+export * from "./types/classes/TypeConfig";
+
+export {
+	Container,
+	ContainerBuilder
+} from "./src";
--- types/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from "./interfaces/TClassOrFactory";
-export * from "./classes/Container";
-export * from "./classes/ContainerBuilder";
-export * from "./classes/TypeConfig";
